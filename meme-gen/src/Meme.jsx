b1a8b5f32b8a6e306d2e3fce92eb9135b1e65c37
Saurav <sauravgamer441@gmail.com>
2024-07-09 09:48:41 +0545

    added randomized image render

--- meme-gen/src/Meme.jsx
+++ meme-gen/src/Meme.jsx
@@ -1,14 +1,24 @@
 import React from "react"
 import "./Meme.css"
+import memesData from "./memesData"
 
 export default function Meme(){
+    const [memeImage, setMemeImage] = React.useState("")
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url)
+    }
+    
     return(
         <main className="main">
             <div>
                 <input type="text" placeholder="Header" className="Header"/>
                 <input type="text" placeholder="Footer" className="Footer"/>
-                <button className="button">Get a new image</button>
+                <button className="button"  onClick={getMemeImage}> Get a new image</button>
             </div>
+            <img  class="image-div" src={memeImage} alt="no image found!" />
         </main>
     )
 }

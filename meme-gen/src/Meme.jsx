36f769ed4adc2625ce58719a4b9755ce8f877dea
Saurav <sauravgamer441@gmail.com>
2024-07-09 14:53:39 +0545

    added useState

--- meme-gen/src/Meme.jsx
+++ meme-gen/src/Meme.jsx
@@ -3,12 +3,22 @@ import "./Meme.css"
 import memesData from "./memesData"
 
 export default function Meme(){
-    const [memeImage, setMemeImage] = React.useState("")
+    const [meme, setMeme] = React.useState(
+         {
+         topText: '',
+         bottomText:'',
+         randomImage: 'http://i.imgflip.com/1bij.jpg'
+        }
+    )
 
     function getMemeImage() {
         const memesArray = memesData.data.memes
         const randomNumber = Math.floor(Math.random() * memesArray.length)
-        setMemeImage(memesArray[randomNumber].url)
+        const url =memesArray[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
     }
     
     return(
@@ -18,7 +28,7 @@ export default function Meme(){
                 <input type="text" placeholder="Footer" className="Footer"/>
                 <button className="button"  onClick={getMemeImage}> Get a new image</button>
             </div>
-            <img  class="image-div" src={memeImage} alt="no image found!" />
+            <img  class="image-div" src={meme.randomImage} alt="no image found!" />
         </main>
     )
 }

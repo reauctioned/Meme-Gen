0fa354f32e8d76f907bd02cc29364128ecce3943
Saurav <sauravgamer441@gmail.com>
2024-07-13 15:07:39 +0545

    deployment success

--- meme-gen/src/Navbar.jsx
+++ meme-gen/src/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(){
       <header className="navbar"> 
       
        <img src="generator.png" alt='meme gen logo' className="navbar--img"></img>
-       <h1>Build a Meme in 6.9 seconds</h1>
+       <h1>Build a Meme in 6.9 seconds (For some tempaltes atm)</h1>
       
 
       </header>

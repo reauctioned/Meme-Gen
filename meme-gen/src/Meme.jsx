42aea3b7c64f7e0402cc1bbd78b5d6f15e8e73e3
Saurav <sauravgamer441@gmail.com>
2024-07-13 13:52:58 +0545

    added meme text

--- meme-gen/src/Meme.jsx
+++ meme-gen/src/Meme.jsx
@@ -10,6 +10,7 @@ export default function Meme(){
          randomImage: 'http://i.imgflip.com/1bij.jpg'
         }
     )
+    
 
     function getMemeImage() {
         const memesArray = memesData.data.memes
@@ -21,14 +22,47 @@ export default function Meme(){
         }))
     }
     
+    function handleChange(event){
+        const {name,value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+    }
+
+
     return(
         <main className="main">
             <div>
-                <input type="text" placeholder="Header" className="Header"/>
-                <input type="text" placeholder="Footer" className="Footer"/>
-                <button className="button"  onClick={getMemeImage}> Get a new image</button>
+                <input 
+                type="text" 
+                placeholder="Header" 
+                className="Header" 
+                name = 'topText'
+                value = {meme.topText}
+                onChange = {handleChange}
+                />
+                
+                <input 
+                type="text" 
+                placeholder="Footer" 
+                className="Footer"
+                name = 'bottomText'
+                value = {meme.bottomText}
+                onChange = {handleChange}
+                />
+
+                <button className="button"  
+                onClick={getMemeImage}
+                > Get a new image</button>
+
             </div>
+
+            <div className = 'meme'>
             <img  class="image-div" src={meme.randomImage} alt="no image found!" />
+              <h2 className="memetextTop">{meme.topText}</h2>
+              <h2 className="memetextBottom">{meme.bottomText}</h2>
+            </div>
         </main>
     )
 }
